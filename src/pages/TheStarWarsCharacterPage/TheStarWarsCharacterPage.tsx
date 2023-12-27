20713a6cf6f2ed1e153b5d0ee313fe60b992b617
alnikif <nikiforiakoleksandr@gmail.com>
2023-12-27 19:41:26 +0100

    add fetch reducer for The Star Wars character page

--- src/pages/TheStarWarsCharacterPage/TheStarWarsCharacterPage.tsx
+++ src/pages/TheStarWarsCharacterPage/TheStarWarsCharacterPage.tsx
@@ -1,48 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
 import { useParams } from 'react-router-dom';
+
 import { StarWarsType } from '../../types/starWarsTypes';
 import { TheStarWarsCharacter } from './TheStarWarsCharacter/TheStarWarsCharacter';
+import { useDispatch, useSelector } from 'react-redux';
+import { DispatchType } from '../../store';
+import { fetchTheStarWarsData } from '../../redux/theStarWarsCharacter/thunkActions';
+import { createStructuredSelector } from 'reselect';
+
+import {
+  selectTheStarWarsCharacterData,
+  selectTheStarWarsCharacterError,
+  selectTheStarWarsCharacterPending,
+  selectTheStarWarsCharacterStatus
+} from '../../redux/theStarWarsCharacter/selectors';
 
 export const TheStarWarsCharacterPage = () => {
-  const [starWarsCharacter, setStarWarsCharacter] = useState<StarWarsType | null>(null);
   const { characterId } = useParams();
-  const [error, setError] = useState<Error | null>(null);
-  const [loading, setLoading] = useState(false);
+
+  const theStarWarsCharacterSelectors = createStructuredSelector({
+    results: selectTheStarWarsCharacterData,
+    status: selectTheStarWarsCharacterStatus,
+    loading: selectTheStarWarsCharacterPending,
+    error: selectTheStarWarsCharacterError
+  });
+
+  const { results, status, loading, error } = useSelector(theStarWarsCharacterSelectors);
+
+  const dispatch: DispatchType = useDispatch();
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/${characterId}.json`)
-      .then((response) => {
-        const { data } = response;
-        console.log(response);
-        setStarWarsCharacter(data);
-      })
-      .catch((apiError: unknown) => {
-        if (apiError instanceof Error) {
-          setError(apiError);
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    dispatch(fetchTheStarWarsData(characterId));
   }, [characterId]);
 
-  console.log(starWarsCharacter);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!starWarsCharacter) {
-    return <div>No data</div>;
-  }
-
-  return (
-    <div>
-      <TheStarWarsCharacter characterData={starWarsCharacter} />
-    </div>
-  );
+  return <div>{results && <TheStarWarsCharacter characterData={results} />}</div>;
 };
